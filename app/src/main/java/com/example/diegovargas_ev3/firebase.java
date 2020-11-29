ec761641c3d9e410ea432133859e365abfc36fdc
DiegoMuss <ghost_student@outlook.es>
2020-11-29 15:58:25 -0300

    FireBase

--- app/src/main/java/com/example/diegovargas_ev3/firebase.java
+++ app/src/main/java/com/example/diegovargas_ev3/firebase.java
@@ -1,17 +1,71 @@
 package com.example.diegovargas_ev3;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.UUID;
+
+import Clases.Clientes;
 
 public class firebase extends AppCompatActivity {
+
+    private EditText Nombre, Destino, Promocion;
+    private Button Boton;
+    FirebaseDatabase  firebase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase);
+
+        inicializarFirebase();
+
+        Nombre = (EditText)findViewById(R.id.nombre);
+        Destino = (EditText)findViewById(R.id.destino);
+        Promocion = (EditText)findViewById(R.id.promocion);
+        Boton = (Button)findViewById(R.id.guardar);
+
+        Boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!Nombre.equals("") & Destino.equals("") & Promocion.equals("")){
+                    Clientes c = new Clientes();
+                    c.setId(UUID.randomUUID().toString());
+                    c.setNombre(Nombre.getText().toString());
+                    c.setDestino(Destino.getText().toString());
+                    c.setPromocion(Promocion.getText().toString());
+
+                    databaseReference.child("Clientes").child(c.getId()).setValue(c);
+
+                    Toast.makeText(getBaseContext(), "Se ha guardado correctamente", Toast.LENGTH_LONG).show();
+
+                    Nombre.setText("");
+                    Destino.setText("");
+                    Promocion.setText("");
+                }
+                else
+                {
+                    Toast.makeText(getBaseContext(), "No se ha guardado correctamente, por favor rellene los campos correctamente", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void inicializarFirebase()
+    {
+        FirebaseApp.initializeApp(this);
+        firebase = FirebaseDatabase.getInstance();
+        databaseReference = firebase.getReference();
     }
 
     public void lista(View v)
--- app/src/main/java/com/example/diegovargas_ev3/listado_clientes.java
+++ app/src/main/java/com/example/diegovargas_ev3/listado_clientes.java
@@ -1,14 +1,91 @@
 package com.example.diegovargas_ev3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Clases.Clientes;
 
 public class listado_clientes extends AppCompatActivity {
+
+    private ListView Lista;
+
+    private List<Clientes> listaClientes = new ArrayList<Clientes>();
+
+    FirebaseDatabase firebase;
+    DatabaseReference databaseReference;
+
+    Clientes seleccion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado_clientes);
+
+        inicialisarBase();
+
+        Lista = (ListView)findViewById(R.id.lista);
+
+        databaseReference.child("Clientes").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot objSnapshot : snapshot.getChildren())
+                {
+                    Clientes c = objSnapshot.getValue(Clientes.class);
+
+                    listaClientes.add(c);
+
+                    ArrayAdapter adapt = new ArrayAdapter(getBaseContext(), android.R.layout.simple_list_item_1, listaClientes);
+                    Lista.setAdapter(adapt);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        Lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                seleccion = (Clientes) adapterView.getItemAtPosition(i);
+            }
+        });
+    }
+
+    public void eliminar(View view)
+    {
+        Clientes c = new Clientes();
+        c.setId(seleccion.getId());
+
+        databaseReference.child("Clientes").child(c.getId()).removeValue();
+
+        Toast.makeText(getBaseContext(), "Cliente eliminado Correctamente", Toast.LENGTH_LONG).show();
+    }
+
+    public void inicialisarBase ()
+    {
+        FirebaseApp.initializeApp(this);
+        firebase = FirebaseDatabase.getInstance();
+        databaseReference = firebase.getReference();
     }
 }
